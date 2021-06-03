e136c31dcc8bf20d2825cdc1945f180d8126a27d
Seires Li <seiresli@pcpartner.com>
2021-06-03 12:02:52 +0800

    debug output and filter some weird peci names

--- start_hwmon.sh
+++ start_hwmon.sh
@@ -3,7 +3,7 @@
 action=$1
 devpath=$2
 of_fullname=$3
-
+echo $1 $2 $3 >> /var/log/hwmon.log
 #Use of_fullname if it's there, otherwise use devpath.
 
 path=$of_fullname
@@ -17,8 +17,19 @@ then
     fi
 fi
 
+#peci devices are also passed in here, but not every one is a valid path
+if [[ $path == *"peci"* ]]; then
+    if [[ $path == *"temp"* ]]; then
+        echo starting $path >> /var/log/hwmon.log
+    else
+        echo not a valid device path $path >> /var/log/hwmon.log
+        exit 0
+    fi
+fi
+
 # Needed to re-do escaping used to avoid bitbake separator conflicts
 path="${path//:/--}"
 # Needed to escape prior to being used as a unit argument
-path="$(systemd-escape "$path")"
-systemctl --no-block "$action" "xyz.openbmc_project.Hwmon@$path.service"
+#path="$(systemd-escape "$path")"
+#systemctl --no-block "$action" "xyz.openbmc_project.Hwmon@$path.service"
+systemctl --no-block "$action" "xyz.openbmc_project.Hwmon@$(systemd-escape $path).service"
